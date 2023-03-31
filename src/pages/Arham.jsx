2159d5eee41e26e7bd1c825f531a834330578c39
arham <201111045@mhs.stiki.ac.id>
2023-03-31 18:33:32 +0700

    arham-test

---
+++ src/pages/Arham.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container } from "react-bootstrap";
+import Homepage from "../components/Homepage";
+
+function Arham() {
+  return (
+    <div><h1>Arham</h1></div>
+  )
+}
+
+export default Arham;
