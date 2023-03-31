930e50481f373dd2670647f95adde67ef1c8928e
Fahmi Alfareza <fahmialfareza@icloud.com>
2023-03-31 17:22:59 +0700

    feature: add homepage

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+function Homepage() {
+  return (
+    <Row>
+      <Col>
+        <h1>Home Page</h1>
+      </Col>
+    </Row>
+  );
+}
+
+export default Homepage;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Homepage from "../components/Homepage";
+
+function Home() {
+  return (
+    <Container>
+      <Homepage />
+    </Container>
+  );
+}
+
+export default Home;
