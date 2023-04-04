5fed28387e79064dda347aadd1f32196803d6dd6
Latifano <85498906+Latifano@users.noreply.github.com>
2023-04-05 06:29:57 +0700

    feature: add pages Romi (#3)
    * add pages Romi
    
    * delete romi from home.jsx

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Arham from "./pages/Arham";
 import Home from "./pages/Home";
+import Romi from "./pages/Romi";
 import AtillaPage from "./pages/AtillaPage";
 import Syuhda from "./pages/Syuhda";
 
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/romi" element={<Romi />} />
         <Route path="/atilla" element={<AtillaPage />} />
         <Route path="/arham" element={<Arham />} />
         <Route path="/syuhda" element={<Syuhda />} />
