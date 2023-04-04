8773b42dbf06deb0067467435c827ddcd15fdc70
syuhda <syuhdafakhrunnisa19@gmail.com>
2023-04-05 06:01:11 +0700

    feature: syuhda page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/arham" element={<Arham />} />
+        <Route path="/syuhda" element={<Syuhda />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Syuhda.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Syuhda() {
+  return (
+    <div>
+      <h1>Hello, I'm Syuhda</h1>
+    </div>
+  );
+}
+
+export default Syuhda;
