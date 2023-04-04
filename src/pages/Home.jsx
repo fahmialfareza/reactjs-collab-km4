9af6484b019319fc6887eb4a11ca03a24923b3a2
msyahdana <msyahdanabdillah@gmail.com>
2023-04-05 06:18:14 +0700

    remove: Syahdan page in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import Homepage from "../components/Homepage";
-import Arham from "./Arham";
-import Syahdan from "./Syahdan";
 
 function Home() {
   return (
     <Container>
       <Homepage />
-      <Arham />
-      <Syahdan />
     </Container>
   );
 }
