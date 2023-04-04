55d8f0d2270d6ee01da53773c58db30efed5a92d
Atilla Silva T <111876471+atillatrisakti@users.noreply.github.com>
2023-04-05 06:25:07 +0700

    feature: atilla page (#6)
    * feature: atilla page
    
    * feature: atilla page
    
    ---------
    
    Co-authored-by: atilla trisakti <atillatrisakti01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Arham from "./pages/Arham";
 import Home from "./pages/Home";
+import AtillaPage from "./pages/AtillaPage";
 import Syuhda from "./pages/Syuhda";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/atilla" element={<AtillaPage />} />
         <Route path="/arham" element={<Arham />} />
         <Route path="/syuhda" element={<Syuhda />} />
       </Routes>
