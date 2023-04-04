f7d99602a86d90791ae33cf9dacc85725c2c5072
lambadya14 <lambadya421@gmail.com>
2023-04-05 08:25:03 +0900

    feature:victor_pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Arham from "./pages/Arham";
 import Home from "./pages/Home";
+import Victor from "./pages/Victor";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/arham" element={<Arham />} />
+        <Route path="/victor" element={<Victor />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Homepage from "../components/Homepage";
 import Arham from "./Arham";
+import Victor from "./Victor";
 
 function Home() {
   return (
     <Container>
       <Homepage />
       <Arham />
+      <Victo/>
     </Container>
   );
 }
