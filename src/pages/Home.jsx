6341ab7511672a35a50303b4d8d7a95cde979c39
atilla trisakti <atillatrisakti01@gmail.com>
2023-04-05 06:12:04 +0700

    feature: atilla page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import Homepage from "../components/Homepage";
-import AtillaPage from "./AtillaPage";
 
 function Home() {
   return (
     <Container>
       <Homepage />
-      <AtillaPage />
     </Container>
   );
 }
