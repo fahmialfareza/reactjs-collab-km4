f7e05989e136f331911b8683179a8c87bbefd1ce
msyahdana <msyahdanabdillah@gmail.com>
2023-04-05 06:08:14 +0700

    feature: Syahdan page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Homepage from "../components/Homepage";
 import Arham from "./Arham";
+import Syahdan from "./Syahdan";
 
 function Home() {
   return (
     <Container>
       <Homepage />
       <Arham />
+      <Syahdan />
     </Container>
   );
 }
