551c7b5be39574f76cfde29d0506caf05a1d2bbf
Daniel <devilwhitedemon@gmail.com>
2023-04-05 06:08:14 +0700

    add:page_daniel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Arham from "./pages/Arham";
 import Home from "./pages/Home";
+import Daniel from "./pages/Daniel";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/arham" element={<Arham />} />
+        <Route path="/daniel" element={<Daniel />} />
       </Routes>
     </BrowserRouter>
   );
