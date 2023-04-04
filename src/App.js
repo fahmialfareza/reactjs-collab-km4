9ee1e3545965038657bb8026fec747382fd0eba7
atilla trisakti <atillatrisakti01@gmail.com>
2023-04-05 06:06:15 +0700

    feature: atilla page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import AtillaPage from "./pages/AtillaPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/atilla" element={<AtillaPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import Homepage from "../components/Homepage";
+import AtillaPage from "./AtillaPage";
 
 function Home() {
   return (
     <Container>
       <Homepage />
+      <AtillaPage />
     </Container>
   );
 }
