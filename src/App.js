e207097a6616f7a3394c9b4efb1b8679c30231d8
Syuhda Fakhrunnisa <75739867+syuhda@users.noreply.github.com>
2023-04-05 06:11:24 +0700

    feature: syuhda page (#2)
    * feature: syuhda page
    
    * feature: import page in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Arham from "./pages/Arham";
 import Home from "./pages/Home";
+import Syuhda from "./pages/Syuhda";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/arham" element={<Arham />} />
+        <Route path="/syuhda" element={<Syuhda />} />
       </Routes>
     </BrowserRouter>
   );
