8a69dee5ee5bc782bb84b547436b71a97a93c387
lambadya14 <lambadya421@gmail.com>
2023-04-05 09:09:11 +0900

    edit Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Homepage from "../components/Homepage";
 import Arham from "./Arham";
-import Victor from "./Victor";
 
 function Home() {
   return (
     <Container>
-      <Homepage />
+      <Homepage/>
       <Arham />
-      <Victo/>
     </Container>
   );
 }
